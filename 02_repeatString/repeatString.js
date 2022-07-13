cd315d62bb5149bc55339924d846e402706b3098
gweryf <priyam2007sp@gmail.com>
2022-07-13 20:47:07 +0530

    solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let s = '';
+    if(num<0){
+        return 'ERROR';
+    }
+    else{
+        for(let i = 0; i<num; i++){
+            s = s+string;
+        }
+        return s;
+    }   
 };
 
 // Do not edit below this line
