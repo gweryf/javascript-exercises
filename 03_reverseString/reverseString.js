69080c94bcd4e68e13c03027ad61f668359a772f
gweryf <priyam2007sp@gmail.com>
2022-07-14 18:11:34 +0530

    Solved!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let wrd2 = '';
+    let n = word.length;
+    for(let i = n-1; i>=0; i--){
+        wrd2 += word[i];
+    }
+    return wrd2;
 };
 
 // Do not edit below this line
