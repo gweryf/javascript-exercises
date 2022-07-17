952aa508760798a3a791fb74ac9e4e15524f57a6
gweryf <priyam2007sp@gmail.com>
2022-07-17 08:59:23 +0530

    Solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...num) {
+    const array = num[0];
+    let arr = [];
+    array.forEach((item) => {
+        if(!num.includes(item)){
+            arr.push(item);
+        }
+    });
+    return arr;
 };
 
 // Do not edit below this line
