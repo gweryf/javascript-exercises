ec8a74b82101471ca33ec52f30307ae74e18e308
gweryf <priyam2007sp@gmail.com>
2022-07-17 11:32:00 +0530

    Solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fah) {
+  let c = Math.round(((fah-32)*5*10)/9)/10;
+  return c;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let f = Math.round((1.8*cel + 32)*10)/10;
+  return f;
 };
 
 // Do not edit below this line
